d72f73d850d3c18a3f5d303c72f9100f482c5751
蔺浪 <mogoson@outlook.com>
2020-03-22 11:27:04 +0800

    Add Transmission demo resources.

---
+++ UnityProject/Assets/MGS-Machinery/Scripts/HUD/DifferentialHUD.cs
@@ -0,0 +1,52 @@
+﻿/*************************************************************************
+ *  Copyright © 2017-2018 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  DifferentialHUD.cs
+ *  Description  :  Draw scene HUD to control differential.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  6/6/2018
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using UnityEngine;
+
+namespace MGS.Machinery
+{
+    [AddComponentMenu("MGS/Machinery/DifferentialHUD")]
+    [RequireComponent(typeof(Differential))]
+    public class DifferentialHUD : MonoBehaviour
+    {
+        #region Field and Property
+        public float top = 10;
+        public float left = 10;
+
+        private Differential differential;
+        private float coefficient = 0;
+        #endregion
+
+        #region Private Method
+        private void Start()
+        {
+            differential = GetComponent<Differential>();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Space(top);
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(left);
+
+            var sliderValue = GUILayout.HorizontalSlider(coefficient, -2, 2, GUILayout.Width(240));
+            if (coefficient != sliderValue)
+            {
+                coefficient = sliderValue;
+                differential.Coefficient = coefficient;
+            }
+
+            GUILayout.EndHorizontal();
+        }
+        #endregion
+    }
+}
---
+++ UnityProject/Assets/MGS-Machinery/Scripts/HUD/EngineHUD.cs
@@ -0,0 +1,51 @@
+﻿/*************************************************************************
+ *  Copyright © 2017-2018 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  EngineHUD.cs
+ *  Description  :  Draw scene HUD to control Engine.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  6/24/2017
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using UnityEngine;
+
+namespace MGS.Machinery
+{
+    [AddComponentMenu("MGS/Machinery/EngineHUD")]
+    [RequireComponent(typeof(Engine))]
+    public class EngineHUD : MonoBehaviour
+    {
+        #region Field and Property
+        public float top = 10;
+        public float left = 10;
+
+        private Engine engine;
+        #endregion
+
+        #region Private Method
+        private void Start()
+        {
+            engine = GetComponent<Engine>();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Space(top);
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(left);
+            if (GUILayout.Button("Turn On Engine"))
+            {
+                engine.TurnOn();
+            }
+            if (GUILayout.Button("Turn Off Engine"))
+            {
+                engine.TurnOff();
+            }
+            GUILayout.EndHorizontal();
+        }
+        #endregion
+    }
+}
